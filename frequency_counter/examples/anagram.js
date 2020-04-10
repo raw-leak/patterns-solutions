a58dd52a8c9e98d5b7517353bee0dd31711064b8
mykhaylo.gusak@between.tech <mykhaylo.gusak@between.tech>
2020-04-10 19:10:45 +0200

    add template and documentation

---
+++ frequency_counter/examples/anagram.js
@@ -0,0 +1,31 @@
+// PROBLEM EXPLANATION
+
+// Given two strings, write a function to determine if the second string is an anagram of the first. 
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as study, formed from dusty.
+
+/**
+ * Anagram comparison/validation
+ * 
+ * @author Mykhaylo Gusak <www.linkedin.com/in/mykhaylo-gusak/>
+ * 
+ * @param {String} string1 - First string to compare
+ * @param {String} string2 - Second string to be compared with
+ * 
+ * @returns {Boolean} If it meet the requirement.
+ */
+
+function validationAnagram(string1, string2) {
+
+    // implement the solution
+
+    return true || false
+}
+
+// OUTPUT
+
+validationAnagram('', '') // true
+validationAnagram('cinema', 'iceman') // true
+validationAnagram("study ", "dusty") // true
+
+validationAnagram('hello', 'hallo') // false
+validationAnagram('323', '123') // false
